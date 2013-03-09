654607bfa0f8535f43a76cfd568047abfe6ecb4c
Phil Pratt-Szeliga <pcpratts@chirrup.org>
2013-03-09 15:42:13 -0500

    first commit

---
+++ src/edu/syr/bytecast/test/BytecastTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.syr.bytecast.test;
+
+/**
+ *
+ * @author pcpratts
+ */
+public class BytecastTest {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
